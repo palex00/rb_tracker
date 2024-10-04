af376e02d198879dca8d417f1276d0ed511d205c
palex00 <32203971+palex00@users.noreply.github.com>
2024-10-04 19:47:52 +0200

    Add files via upload

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -6,7 +6,13 @@
 
 ScriptHost:LoadScript("scripts/autotracking/slot_options.lua")
 ScriptHost:LoadScript("scripts/autotracking/item_mapping.lua")
-ScriptHost:LoadScript("scripts/autotracking/location_mapping.lua")
+
+
+if IS_PSEUDOTRACKING then
+	ScriptHost:LoadScript("scripts/autotracking/location_and_event_mapping.lua")
+	else
+	ScriptHost:LoadScript("scripts/autotracking/location_mapping.lua")
+end
 
 CUR_INDEX = -1
 SLOT_DATA = nil
